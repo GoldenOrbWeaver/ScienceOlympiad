2d7d32e3b09d04210f3fc9b4b18b635d219b5811
GoldenOrbWeaver <photographerrenoite@gmail.com>
2025-11-21 10:06:30 -0800

    Mostly finished Controller, will add to later though

--- ElectricVehicleMicropython/electricvehiclemicropython.py
+++ ElectricVehicleMicropython/electricvehiclemicropython.py
@@ -10,6 +10,7 @@ version: Micropython
 from machine import Pin, PWM
 from utime import sleep
 from math import asin
+import time
 
 # Setting up pins
 motor_encoder=Pin(0, Pin.IN)
@@ -20,26 +21,29 @@ ir_encoder=Pin(16, Pin.IN)
 
 
 # Run variables (get these to run after inputs but before start)
-run_time=10
-nominal_distance=700
+run_time=10 # time in s
+nominal_distance=700 # distance in cm
 radius=(80**2+nominal_distance**2/4)/(2*80)
 run_distance=2*radius*asin(nominal_distance/(2*radius))
 time_a=0.5*(run_time-((0.5*run_time**2-4*run_distance)**0.5)/(0.8**0.5))
 
 # Encoder loop
 class Encoder:
-    def __init__(self):
+    def __init__(self, interval: int):
         self.old_distance=0
         self.distance=0 # Distance in cm
         self.speed=0 # Speed in cm/s
         self.old_value=0
         self.i=0
+        self.interval=interval
 
     def update(self):
         if self.old_value!=motor_encoder.value():
             self.distance+=0.221656815
+        if i%100==0: #Check if 100 works or if you need to change this
+            self.speed=(self.distance-self.old_distance)/(self.interval*100)
+            self.old_distance=self.distance
         i+=1
-        #ADD STUFF FOR SPEED
     
     def getDistance(self):
         return(self.distance)
@@ -49,12 +53,23 @@ class Encoder:
     
 # PI loop
 class Controller():
-    def __init__(self, encoder):
+    def __init__(self, encoder: Pin, interval: int, start):
         self.k_p=1
         self.k_i=1
         self.error=0
         self.error_sum=0
         self.encoder=encoder
+        self.interval=interval
+        self.start=start
 
     def compute(self):
-        self.error+=self.encoder.getDistance()-run_time
+        time_elapsed=time.ticks_diff(time.ticks_ms(), self.start)
+        if time_elapsed<=time_a*1000: # Accelerating section
+            self.error=0.5*0.5*((time_elapsed/1000)**2)-self.encoder.getDistance()
+        elif time_elapsed>time_a*1000 and time_elapsed<(run_time-time_a)*1000: # Constant velocity section
+            self.error=0.5*0.5*time_a**2+(time_elapsed/1000-time_a)*time_a*0.5-self.encoder.getDistance()
+        elif time_elapsed>=(run_time-time_a)*1000: # Decelerating section
+            self.error=0.5*0.5*time_a**2+(time_elapsed/1000-time_a)*time_a*0.5-0.5*0.5*(time_a-run_time+time_elapsed/1000)**2-self.encoder.getDistance()
+        else: # After time
+            self.error=run_distance-self.encoder.getDistance()
+        self.error_sum+=self.error
--- TestProject/TestProject/main.py
+++ TestProject/TestProject/main.py
@@ -1,5 +1,13 @@
-from machine import Pin
+from machine import Pin, PWM
 from utime import sleep
+
+#Setting up pins
+motor_pwm=PWM(Pin(28, Pin.OUT))
+motor_pwm.freq(20000)
+motor_pwm.duty_u16(0)
+motor_encoder=Pin(26, Pin.IN)
+motor_direction=Pin(27, Pin.OUT)
+motor_direction.on()
 
 encoder = Pin(16, Pin.IN, Pin.PULL_UP)
 past_value=0
@@ -9,7 +17,7 @@ i=0
 old_distance=0
 
 while(True):
-    current_value=1-encoder.value()
+    current_value=1-motor_encoder.value()
     if current_value!=past_value:
         distance+=0.7854
     #print("Distance is "+str(distance))
@@ -17,6 +25,7 @@ while(True):
         print("Speed is "+str((distance-old_distance)/0.100)+"cm/s")
         print("Distance is "+str(distance))
         old_distance=distance
+        motor_pwm.duty_u16(65525*int((-1)**(i/100)))
     past_value=current_value
     i+=1
     sleep(0.001)
